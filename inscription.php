3645fe1584c874300d4d6a0ad5f65a8a7de177f7
Romain <romain.moret@outlook.com>
2019-04-24 15:01:01 +0200

    Adding database queries, checking all the registration form

--- inscription.php
+++ inscription.php
@@ -8,6 +8,59 @@ catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
 }
+if(!empty($_POST))
+{
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $mail2 = htmlspecialchars($_POST['mail2']);
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+    if(!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['mail2']) && !empty($_POST['password']) && !empty($_POST['password2']))
+    {
+        $pseudoLength = strlen($pseudo);
+        if($pseudoLength <= 25)
+        {
+            $reqPseudo = $bdd->prepare("SELECT * FROM membres WHERE pseudo = ?");
+            $reqPseudo->execute(array($pseudo));
+            $pseudoExist = $reqPseudo->rowCount();
+            if($pseudoExist === 0)
+            {
+                if($mail === $mail2)
+                {
+                    if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+                    {
+                        $reqMail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
+                        $reqMail->execute(array($mail));
+                        $mailExist = $reqMail->rowCount();
+                        if($mailExist === 0)
+                        {
+                            if($password === $password2)
+                            {
+                                $passHash = password_hash($password, PASSWORD_DEFAULT);
+                                $insertMember = $bdd->prepare("INSERT INTO membres (pseudo, mail, password) VALUES (?, ?, ?)");
+                                $insertMember->execute(array($pseudo, $mail, $passHash));
+                                $success = "Votre compte a bien été créé !";
+                            }
+                        }
+                        else
+                            $message = "Adresse mail déjà utilisée !";
+                    }
+                    else
+                        $message = "Votre adresse mail n'est pas valide.";
+                }
+                else
+                    $message = "Vos adresses mail ne correspondent pas !";
+
+            }
+            else
+                $message = "Le pseudo existe déjà !";
+        }
+        else
+            $message = "Votre pseudo ne doit pas dépasser 25 caractères.";
+    }
+    else
+        $message = "Tous les champs doivent être complétés !";
+}
 ?>
 <!doctype html>
 <html lang="fr">
@@ -34,16 +87,16 @@ catch(Exception $e)
         <form action="inscription.php" method="post">
             <div class="form-group">
                 <label for="pseudo">Pseudo</label>
-                <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Votre pseudo" requiered>
+                <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Votre pseudo" value="<?php if(isset($pseudo)){echo $pseudo;} ?>" requiered>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="mail">Mail</label>
-                    <input type="mail" class="form-control" name="mail" id="mail" placeholder="Votre mail" requiered>
+                    <input type="mail" class="form-control" name="mail" id="mail" placeholder="Votre mail" value="<?php if(isset($mail)){echo $mail;} ?>" requiered>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="mail2">Confirmation du mail</label>
-                    <input type="mail" class="form-control" name="mail2" id="mail2" placeholder="Confirmation de votre mail" requiered>
+                    <input type="mail" class="form-control" name="mail2" id="mail2" placeholder="Confirmation de votre mail" value="<?php if(isset($mail2)){echo $mail2;} ?>" requiered>
                 </div>
             </div>
 
@@ -56,6 +109,14 @@ catch(Exception $e)
                     <label for="password2">Confirmation mot de passe</label>
                     <input type="password" class="form-control" name="password2" id="password2" placeholder="Confirmation de votre mot de passe" requiered>
                 </div>
+                <!-- Erreur -->
+                <?php if(isset($message)){ ?>
+                <p class="text-danger"><?= $message ?></p>
+                <?php } ?>
+                <!-- Succès -->
+                <?php if(isset($success)){ ?>
+                <p class="text-success"><?= $success ?></p>
+                <?php } ?>
             </div>
             <button type="submit" class="btn btn-primary">Je m'inscris</button>
             <p class="mt-2">Vous avez un compte ? <a href="connexion.php">Se connecter</a></p>
