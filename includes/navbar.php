0567e34d86206f131b3c5a4eaa4b91f2fa687013
Romain <romain.moret@outlook.com>
2019-05-20 16:51:52 +0200

    Adding a banner to accept cookies

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,8 +1,18 @@
 <?php
-      // On récupère l'URL de la page pour ensuite affecter class = "active" aux liens de nav
-      $page = $_SERVER['REQUEST_URI'];
-      $page = str_replace("/projet_4/", "", $page);
+// On récupère l'URL de la page pour ensuite affecter class = "active" aux liens de nav
+$page = $_SERVER['REQUEST_URI'];
+$page = str_replace("/projet_4/", "", $page);
+if(isset($_COOKIE['accept_cookie']))
+    $showCookie = false;
+else
+    $showCookie = true;
 ?>
+<?php if($showCookie){ ?>
+<div class="alert alert-light alert-dismissible fade show mb-0 align-items-center rounded-0 p-1 flex-column fixed-bottom text-center" role="alert">
+    <p class='my-0'>En poursuivant votre navigation, vous <b>acceptez</b> l’utilisation de <b>cookies</b> pour vous offrir une meilleure expérience utilisateur.</p>
+    <a class="btn btn-secondary my-1 btn-sm align-self-center" href="accept_cookie.php">Accepter</a>
+</div>
+<?php } ?>
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark <?php if($page === 'index.php' || $page === ''){echo ' bg-transparent';} else echo ' bg-dark'; ?>">
     <a class="navbar-brand" href="index.php">Jean Forteroche</a>
@@ -16,9 +26,6 @@
             </li>
             <li class="nav-item  <?php if($page === "chapters.php" || $page === "chapter.php?id=" . $_GET['id']){echo ' active';} ?>">
                 <a class="nav-link" href="chapters.php"><i class="fa fa-book"></i> Chapitres</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fa fa-info-circle"></i> À propos</a>
             </li>
         </ul>
         <ul class="navbar-nav">
